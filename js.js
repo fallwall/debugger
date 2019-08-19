fce5f4feb66130169964c397da036e84528a518e
XX <xx@XXs-MBP13.local>
2019-08-18 23:53:01 -0400

    assets added

---
+++ js.js
@@ -0,0 +1,35 @@
+
+
+const images = [
+  "assets/Alex01.jpg",
+  "assets/Alex02.jpg",
+  "assets/Alex03.jpg",
+  "assets/Alex04.jpg",
+  "assets/Alex05.jpg",
+  "assets/Alex06.jpg",
+  "assets/Alex07.jpg",
+  "assets/Alex08.jpg",
+  "assets/Alex09.jpg",
+  "assets/Alex010.jpg",
+  "assets/Alex011.jpg",
+  "assets/Alex012.jpg",
+  "assets/Alex013.jpg",
+  "assets/Alex014.jpg",
+  "assets/Alex015.jpg",
+  "assets/Alex016.jpg",
+  "assets/Alex017.jpg",
+  "assets/Alex018.jpg",
+  "assets/Alex019.jpg",
+  "assets/Alex020.jpg",
+  "assets/Alex021.jpg",
+  "assets/Alex022.jpg",
+  "assets/Alex023.jpg",
+  "assets/Alex024.jpg",
+  "assets/Alex025.jpg",
+  "assets/Alex026.jpg",
+  "assets/Alex027.jpg",
+  "assets/Alex028.jpg",
+  "assets/Alex029.jpg",
+  "assets/Alex030.jpg"
+]
+
