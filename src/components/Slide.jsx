c1b9184469047280cdbb41be9c5d2f0dd0e79062
XX <fallwall19@gmail.com>
2019-08-29 21:49:46 -0400

    f jquery, updated to react version

---
+++ src/components/Slide.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Slide = ({ image }) => {
+  return (
+    <div className="slide" >
+      <img className="slide-current" src={image} />
+    </div>
+  ) 
+}
+
+export default Slide;
---
+++ src/components/Slider.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import Slide from './Slide';
+import RightArrow from './RightArrow';
+import LeftArrow from './LeftArrow';
+
+const allimages = [
+  'https://i.imgur.com/HatWGp5.jpg',
+  'https://i.imgur.com/T71L0rO.jpg',
+  'https://i.imgur.com/Dep1uaL.jpg',
+  'https://i.imgur.com/XjmAuXZ.jpg',
+  'https://i.imgur.com/fCWGjRn.jpg',
+  'https://i.imgur.com/7DBloS4.jpg',
+  'https://i.imgur.com/HmVmVIn.jpg',
+  'https://i.imgur.com/IL1K0ra.jpg',
+  'https://i.imgur.com/3QNuL6H.jpg',
+  'https://i.imgur.com/K3FNzpA.jpg',
+  'https://i.imgur.com/9S8Qmso.jpg',
+  'https://i.imgur.com/Oh85V12.jpg',
+  'https://i.imgur.com/eHzcpIT.jpg',
+  'https://i.imgur.com/8WaGYGG.jpg',
+  'https://i.imgur.com/0FxIliI.jpg',
+  'https://i.imgur.com/0J7ZBHK.jpg',
+  'https://i.imgur.com/i5FNISD.jpg',
+  'https://i.imgur.com/biYjiqt.jpg',
+  'https://i.imgur.com/qgxqBO9.jpg',
+  'https://i.imgur.com/GLmRUNB.jpg',
+  'https://i.imgur.com/EW60cfT.jpg',
+  'https://i.imgur.com/GEyM4OU.jpg',
+  'https://i.imgur.com/3WOWsX4.jpg',
+  'https://i.imgur.com/vmeOD9r.jpg',
+  'https://i.imgur.com/gu0O8Os.jpg',
+  'https://i.imgur.com/JivyMRZ.jpg',
+  'https://i.imgur.com/hMfKJCr.jpg',
+  'https://i.imgur.com/5r0VWeL.jpg',
+  'https://i.imgur.com/GvW2ntq.jpg',
+  'https://i.imgur.com/Y8A8Us4.jpg'
+]
+
+export default class Slider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      currentIndex: 0
+    }
+  }
+
+  shuffle = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  componentDidMount = () => {
+    const images = this.shuffle(allimages);
+    this.setState({
+      images: images
+    })
+  }
+
+
+  nextSlide = () => {
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex + 1
+    }))
+  }
+
+  prevSlide = () => {
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex - 1
+    }))
+  }
+
+  render() {
+    return (
+      <div className="slider">
+        <p>slider</p>
+        <div className="slider-wrapper">
+          <Slide image={this.state.images[this.state.currentIndex]} />
+        </div>
+      <LeftArrow
+        prevSlide={this.prevSlide} />
+      <RightArrow
+        nextSlide={this.nextSlide} />
+      </div >
+    )
+  }
+}
